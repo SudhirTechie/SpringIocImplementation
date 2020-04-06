ef8d2c97056f6162d3e5fc8780a1cdecb4ffaa53
Sudhir Ranjan Rout <javaangular010@gmail.com>
2020-04-06 18:22:47 +0530

    Implementing ApplicationContext

--- src/main/java/com/sudhir/SpringIoc/App.java
+++ src/main/java/com/sudhir/SpringIoc/App.java
@@ -2,6 +2,9 @@ package com.sudhir.SpringIoc;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 
 /**
@@ -12,8 +15,15 @@ public class App
 {
     public static void main( String[] args )
     {
-    	BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-        State state = (State)factory.getBean("state");
+    	//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+    	
+    	
+    	//Since Bean Factory is now deprectated so we will see how to implement the advance version 
+    	//ApplicationContext now Comes into Picture
+    	//I am commenting BeanFactory and same function we can see using ApplicationContext
+        
+    	ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
+    	State state = (State)context.getBean("state");
         state.name();
     }
 }
